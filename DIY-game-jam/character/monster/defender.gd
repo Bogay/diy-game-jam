06d2fc6836d83ff3ca8ab1a4b1a9d9f0cc52d9ec
bogay <pojay11523@gmail.com>
2021-08-14 14:16:53 +0800

    Detect attackers entered area

--- DIY-game-jam/character/monster/defender.gd
+++ DIY-game-jam/character/monster/defender.gd
@@ -12,6 +12,7 @@ var magic_defense: Buffable
 var attack_distance: Buffable
 # Attack speed
 var speed: Buffable
+var detected_attackers = {}
 onready var attack_area: Area2D = $AttackArea
 onready var attack_shape: CollisionShape2D = $AttackArea/CollisionShape2D
 
@@ -28,9 +29,23 @@ func _ready():
 	attack_distance = Buffable.new(defender_data.attack_distance)
 	(attack_shape.shape as CircleShape2D).radius = attack_distance.value()
 	attack_distance.connect("value_changed", self, "_on_attack_distance_changed")
+	attack_area.connect("body_entered", self, "_on_body_entered")
+	add_to_group("defender")
+
 
 func _on_attack_distance_changed(dis):
 	(attack_shape.shape as CircleShape2D).radius = dis
 
-#func _process(delta):
-#	pass
+
+func _on_body_entered(a: Node):
+	var attacker = a.get_parent()
+	if not attacker is Attacker:
+		return
+	enqueue_attacker(attacker)
+
+
+func enqueue_attacker(attacker: Attacker):
+	var attacker_id = attack.get_instance_id()
+	assert(not attacker_id in detected_attackers)
+	detected_attackers[attacker_id] = attacker
+	print('Got you: ', attacker.name)
