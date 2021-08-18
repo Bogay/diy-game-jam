07e1a37fac9ee83a2b3fec40d7722353b341a1bd
bogay <pojay11523@gmail.com>
2021-08-19 02:07:09 +0800

    Add ImageViewer
    Images will not commit becasue its so large (about 1G).

---
+++ DIY-game-jam/opening/image_viewer.gd
@@ -0,0 +1,38 @@
+class_name ImageViewer
+extends Node
+
+signal play_finished
+signal clicked
+
+
+export(String, DIR) var image_dir_path
+# MAGIC
+export(float) var scale = 0.75
+onready var sprite: Sprite = $Sprite
+
+func _ready():
+	if not image_dir_path.ends_with("/"):
+		image_dir_path += "/"
+	sprite.set_scale(Vector2(scale, scale))
+	play()
+		
+
+func play():
+	var image_dir = Directory.new()
+	image_dir.open(image_dir_path)
+	image_dir.list_dir_begin()
+	while true:
+		var image_name = image_dir.get_next()
+		if image_name == "":
+			break
+		if image_name.ends_with(".png"):
+			print(image_name)
+			var image = load(image_dir_path + image_name)
+			sprite.texture = image
+			yield(self, "clicked")
+	print("End of image view")
+
+
+func _unhandled_input(_event: InputEvent):
+	if Input.is_action_pressed('click'):
+		emit_signal("clicked")
