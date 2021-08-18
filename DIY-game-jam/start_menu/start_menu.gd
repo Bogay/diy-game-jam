765f466f36f9d9fbe1cac5ea54b11726c8cfa757
bogay <pojay11523@gmail.com>
2021-08-19 01:18:30 +0800

    Add start menu

---
+++ DIY-game-jam/start_menu/start_menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+onready var start_button: Button = $Start
+
+func _ready():
+	assert(start_button.connect("pressed", self, "game_start") == OK)
+
+
+func game_start():
+	print("Are you ready?")
