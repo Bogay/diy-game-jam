4c4c11f719434c7ec74c7fce3e26bc900b970fe5
bogay <pojay11523@gmail.com>
2021-08-21 03:08:01 +0800

    Add animation for tentacle shooter

--- DIY-game-jam/character/monster/defender.gd
+++ DIY-game-jam/character/monster/defender.gd
@@ -14,8 +14,11 @@ var attack_distance: Buffable
 var speed: Buffable
 var detected_attackers = {}
 var can_attack = 1
+var original_muzzle_x: float = 0
+onready var muzzle: Node2D = $Muzzle
 onready var attack_area: Area2D = $AttackArea
 onready var attack_shape: CollisionShape2D = $AttackArea/CollisionShape2D
+onready var animated_sprite: AnimatedSprite = $AnimatedSprite
 
 func _ready():
 	assert(defender_data is DefenderData)
@@ -32,6 +35,8 @@ func _ready():
 	assert(attack_distance.connect("value_changed", self, "_on_attack_distance_changed") == OK)
 	assert(attack_area.connect("area_entered", self, "_on_area_entered") == OK)
 	assert(attack_area.connect("area_exited", self, "_on_area_exited") == OK)
+	animated_sprite.animation = "idle"
+	original_muzzle_x = muzzle.position.x
 	add_to_group("defender")
 
 
@@ -40,13 +45,13 @@ func _process(_delta: float):
 	try_shoot()
 
 
-
 func update_direction():
 	var target = get_target_attacker()
 	if target == null:
 		return
 	var is_left = (target.global_position - global_position).x < 0
-	($AnimatedSprite as AnimatedSprite).flip_h = is_left
+	animated_sprite.flip_h = is_left
+	muzzle.position.x = -original_muzzle_x if is_left else original_muzzle_x
 
 
 func try_shoot():
@@ -63,10 +68,21 @@ func shoot() -> void:
 	var target = get_target_attacker()
 	assert(target.connect("tree_exiting", bullet, "_on_target_exiting") == OK)
 	bullet.target = target
-	add_child(bullet)
+	muzzle.add_child(bullet)
+	play_attack_animation()
 	can_attack = 0
 	yield(get_tree().create_timer(1 / speed.value()), "timeout")
 	can_attack = 1
+
+
+func play_attack_animation():
+	animated_sprite.animation = "attack"
+	assert(animated_sprite.connect("animation_finished", self, "attack_animation_callback") == OK)
+	
+
+func attack_animation_callback():
+	animated_sprite.disconnect("animation_finished", self, "attack_animation_callback")
+	animated_sprite.animation = "idle"
 
 
 func get_target_attacker() -> Attacker:
