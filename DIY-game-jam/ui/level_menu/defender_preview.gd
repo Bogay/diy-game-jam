b6cceed10846da610e2ac2001a791ca6f0581428
bogay <pojay11523@gmail.com>
2021-08-22 03:22:50 +0800

    Press defender preview updates player selection

--- DIY-game-jam/ui/level_menu/defender_preview.gd
+++ DIY-game-jam/ui/level_menu/defender_preview.gd
@@ -7,10 +7,14 @@ onready var sprite: TextureRect = $Sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	assert(connect("pressed", self, "set_selection") == OK)
 	add_to_group("defender_preview")
-	
+
 
 func set_defender(new_defender: DefenderData):
 	defender_data = new_defender
 	sprite.texture = defender_data.preview
-	
+
+
+func set_selection():
+	Player.selected_character = defender_data
