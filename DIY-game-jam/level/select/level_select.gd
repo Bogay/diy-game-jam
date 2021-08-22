79688c2d62ac53a17c12fd8bd53d944213d6b909
bogay <pojay11523@gmail.com>
2021-08-23 02:46:55 +0800

    Add close button to select level page

--- DIY-game-jam/level/select/level_select.gd
+++ DIY-game-jam/level/select/level_select.gd
@@ -4,10 +4,11 @@ extends Node
 onready var story_btn: BaseButton = $UIRoot/VSplitContainer/VBoxContainer/StoryButton
 onready var level1_btn: BaseButton = $UIRoot/LevelSelectButtons/level1/level1
 onready var level2_btn: BaseButton = $UIRoot/LevelSelectButtons/level2/level2
-
+onready var option_btn: BaseButton = $UIRoot/AspectRatioContainer/Settings
 
 func _ready():
 	assert(story_btn.connect("pressed", self, "load_story_select") == OK)
+	assert(option_btn.connect("pressed", self, "show_popup") == OK)
 	var save: GameSave = GameSaveManager.current_save
 	if "level1" in save.levels:
 		level1_btn.disabled = false
@@ -27,3 +28,7 @@ func load_level2():
 
 func load_story_select():
 	Game.change_scene("story_select")
+
+
+func show_popup():
+	get_tree().quit()
