b1e9a18a0ad123d9053d25b19ac9d4dcb5c037fd
bogay <pojay11523@gmail.com>
2021-08-23 01:21:25 +0800

    Implement back button for story select page

--- DIY-game-jam/story/select/story_select.gd
+++ DIY-game-jam/story/select/story_select.gd
@@ -5,12 +5,14 @@ extends Node
 onready var opening_btn: BaseButton = $UIRoot/StorySelectButtons/opening/button
 onready var level1_btn: BaseButton = $UIRoot/StorySelectButtons/level1/button
 onready var level2_btn: BaseButton = $UIRoot/StorySelectButtons/level2/button
+onready var back_btn: BaseButton = $UIRoot/AspectRatioContainer/Back
 
 
 func _ready():
 	assert(opening_btn.connect("pressed", self, "go_opening") == OK)
 	assert(level1_btn.connect("pressed", self, "go_level1") == OK)
 	assert(level2_btn.connect("pressed", self, "go_level2") == OK)
+	assert(back_btn.connect("pressed", self, "go_back") == OK)
 
 
 func go_opening():
@@ -23,3 +25,7 @@ func go_level1():
 
 func go_level2():
 	Game.change_scene("level2_win", "story_select")
+
+
+func go_back():
+	Game.change_scene("level_select")
